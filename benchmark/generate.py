16132f48c217502b461d3df84dfb26505b242c39
Ismail Badawi <isbadawi@gmail.com>
2011-10-07 04:12:00 +0000

    initial wig benchmark commit
    git-svn-id: svn+ssh://svn.cs.mcgill.ca/2011/groups/group-d/wig@10573 6669c6d1-5a39-0410-bbc6-daf4d3d1134a

---
+++ benchmark/generate.py
@@ -0,0 +1,32 @@
+import sys
+import random
+
+def valid(word, length):
+    return (len(word) == length and word.isalpha() and word.islower()
+                                and len(word) == len(set(word)))
+
+def generate_word_func(words):
+    func = 'tuple Word getWord(int n) {\n        tuple Word result;\n'
+    func += '        result = initWord();\n'
+    for index, word in enumerate(words):
+        s = '        if (n == %d) {\n' % index
+        for i, l in enumerate(word):
+            s += '            result.index%s = %d;\n' % (l, i)
+        s += '        }\n'
+        func += s
+    func += '        return result;\n    }\n'
+    return func
+    
+if __name__ == '__main__':
+    usage = 'usage: python generate.py word-length amount-to-generate'
+    if len(sys.argv) != 3:
+        print usage
+        sys.exit(1)
+    length = int(sys.argv[1])
+    amount = int(sys.argv[2])
+    with open('/usr/share/dict/words', 'r') as f:
+        words = [w.upper() for w in f.read().split() if valid(w, length)]
+        words = random.sample(words, amount)
+    with open('hangman.wig', 'w') as out, open('hangman.stub', 'r') as stub:
+        print >>out, stub.read().replace('<<W>>', generate_word_func(words))
+
