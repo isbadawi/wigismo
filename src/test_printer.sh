67fef78779826a46cfd17e52ce12a3c27a3f2570
Ismail Badawi <isbadawi@gmail.com>
2011-10-24 04:41:23 +0000

    two more test cases
    git-svn-id: svn+ssh://svn.cs.mcgill.ca/2011/groups/group-d/wig@11483 6669c6d1-5a39-0410-bbc6-daf4d3d1134a

--- src/test_printer.sh
+++ src/test_printer.sh
@@ -6,8 +6,8 @@ fail=0
 echo "*** Testing pretty printer. No output is expected. ***"
 for testcase in `ls $TESTDIR`; do
     if [ -f $TESTDIR/$testcase ]; then
-        wigismo -p $TESTDIR/$testcase > test1.wig
-        wigismo -p test1.wig > test2.wig
+        wigismo -p -s $TESTDIR/$testcase > test1.wig
+        wigismo -p -s test1.wig > test2.wig
         if ! cmp --silent test1.wig test2.wig; then
             echo "pretty(parse(x)) != pretty(parse(pretty(parse(x))))"
             echo "for x = $TESTDIR/$testcase"
