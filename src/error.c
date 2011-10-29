4519db48ed63fe25f637d5c2a914115879607085
Carla Morawicz <coca_carla@hotmail.com>
2011-10-29 22:46:31 +0000

    type.c further implemented
    git-svn-id: svn+ssh://svn.cs.mcgill.ca/2011/groups/group-d/wig@11684 6669c6d1-5a39-0410-bbc6-daf4d3d1134a

--- src/error.c
+++ src/error.c
@@ -20,4 +20,11 @@ void reportStrError(const char *msg, char *name, int lineno)
     fprintf(stderr, msg, name);
     fprintf(stderr, " at line %i of file %s\n", lineno, infile);
     errors++;
-} 
+}
+
+void reportError(char *s, int lineno)
+{
+    fprintf(stderr, "*** ");
+    fprintf(stderr, "%s at line %i\n", s, lineno);
+    errors++;
+}
--- src/type.c
+++ src/type.c
@@ -19,50 +19,22 @@ void initTypes()
     stringTYPE = makeTYPEstring();
 }
 
-int equalTYPE(EXP *s, EXP *t)
+int equalTYPE(TYPE *s, TYPE *t)
 {
-    if (s->type->kind != t->type->kind) return 0;
-    if (s->type->kind == tupleK)
+    if (s->kind != t->kind) return 0;
+    if (s->kind == tupleK)
     {
-        if (s->type->name != NULL && t->type->name != NULL) //TODO set/tupl/exp/name/null
-        {
-            if (strcmp(s->type->name, t->type->name) == 0)
-                return 1;
-            else
-            {
-                return compareSchemaStructuralEquivalence(s, t);
-            }
-        }
-        else if (s->type->name == NULL && t->type->name != NULL)
-        {
-
-        }
-        else if (s->type->name != NULL && t->type->name == NULL)
-        {
-        }
-        else
-            return verifyTupleEquivalence(s, t);
+        if (strcmp(s->name, t->name) == 0)
+            return 1;
+        return compareSchemaStructuralEquivalence(s, t);
     }
     return 1;
 }
 
-int compareSchemaToTuple(EXP *s, EXP *t)
+int compareSchemaStructuralEquivalence(TYPE *s, TYPE *t)
 {
-    SCHEMA *schema = get_symbol(mst, s->type->name)->val.schemaS;
-
-    VARIABLE *currV = schema->variables;
-    while (currV != NULL)
-    {
-        if(!has
-        currV = currV->next;
-    }
-
-}
-
-int compareSchemaStructuralEquivalence(EXP *s, EXP *t)
-{
-    SCHEMA *schemaS = get_symbol(mst, s->type->name)->val.schemaS;
-    SCHEMA *schemaT = get_symbol(mst, t->type->name)->val.schemaS;
+    SCHEMA *schemaS = get_symbol(mst, s->name)->val.schemaS;
+    SCHEMA *schemaT = get_symbol(mst, t->name)->val.schemaS;
 
     VARIABLE *currVariableS = schemaS->variables;
     while(currVariableS != NULL)
@@ -86,62 +58,25 @@ int hasVariableInSchema(VARIABLE *v, SCHEMA *s)
             else
                 return 0;
         }
+        currV = currV->next;
     }
     return 0;
 }
 
-int verifyTupleEquivalence(EXP *s, EXP *t)
+void checkBOOL(TYPE *t, int lineno)
 {
-    if (s->type->kind != tupleK || t->type->kind != tupleK)
-        return 0;
-
-    FIELDVALUE * currFieldValueS = s->val.tupleE.fieldValues;
-    while (currFieldValueS != NULL)
+    if(t->kind != boolK)
     {
-        if (!hasFieldValue(currFieldValueS, t))
-            return 0;
-        currFieldValueS = currFieldValue->next;
+        reportError("bool type expected", lineno);
     }
-    return 1;
 }
 
-//TODO fill in types of fieldvalues in tuples
-
-int hasFieldValue(FIELDVALUE *s, EXP *t)
-{
-    FIELDVALUE * currFieldValueT = t->val.tupleE.fieldValues;
-    while(currFiledValueValueT != NULL)
-    {
-        if(strcmp(s->name, currFieldValueT->name)==0)
-        {
-            if(s->exp->type->kind == currFieldValueT->exp->type->kind)
-                return 1;
-            else
-                return 0;
-        }
-        currFieldValueT = currFieldValueT->next;
-    }
-    return 0;
-}
-
-int checkBOOL(TYPE *t, int lineno)
-{
-    if(t->kinf != boolK)
-    {
-        reportError("char type expected", lineno); //TODO reportError
-        return 0;
-    }
-    return 1;
-}
-
-int checkINT(TYPE *t, int lineno)
+void checkINT(TYPE *t, int lineno)
 {
     if(t->kind != intK)
     {
         reportError("int type expected", lineno);
-        return 0;
     }
-    return 1;
 }
 
 void typeSERVICE(SERVICE *s)
@@ -160,32 +95,50 @@ void typeFUNCTION(FUNCTION *f)
     typeSTATEMENT(f->statements, f->returntype, NULL);
 }
 
-void typeSTATEMENT(STATEMENT *s, TYPE *returntype, SymbolTable *table)
+void typeSTATEMENT(STATEMENT *s, TYPE *returntype)
 {
     if(s == NULL)
         return;
-    typeSTATEMENT(s->next, returntype, table);
     
     switch(s->kind)
     {
         case skipK:
             break;
         case sequenceK:
-            typeSTATEMENT(s->val.sequenceS.first, returntype, table);
-            typeSTATEMENT(s->val.sequenceS.second, returntype, table);
+            typeSTATEMENT(s->val.sequenceS.first, returntype);
+            typeSTATEMENT(s->val.sequenceS.second, returntype);
         case showK:  //TODO always accept strings?
         case exitK:
             break;
         case returnK:
-            if(s->val.returnS != NULL)
-
-            if(s->val.returnS->type->kind == )
+            if(returntype->kind != voidK)
             {
-                if(returnt)
+                if(!equalTYPE(s->val.returnS->type, returntype))
+                {
+                    reportError("wrong return type", s->lineno);
+                }
             }
+            break;
         case blockK:
+            typeSTATEMENT(s->val.blockS.body, returntype);
+            break;
+        case ifK:
+            checkBOOL(s->val.whileS.condition->type, s->lineno);
+            typeSTATEMENT(s->val.ifS.body, returntype);
+            break;
+        case ifelseK:
+            checkBOOL(s->val.whileS.condition->type, s->lineno);
+            typeSTATEMENT(s->val.ifelseS.thenpart, returntype);
+            typeSTATEMENT(s->val.ifelseS.elsepart, returntype);
+            break;
         case whileK:
+            checkBOOL(s->val.whileS.condition->type, s->lineno);
+            typeSTATEMENT(s->val.whileS.body, returntype);
+            break;
         case expK:
+            typeEXP(s->val.expS);
+            break;
+
     }
 }
 
